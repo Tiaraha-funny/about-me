220e90e2914fa617ea7372ed868aa43907db544f
Peta Jeannie <peta.jea@onja.org>
2020-12-14 14:14:19 -0600

    start doing the html

---
+++ Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import mePng from "../images/peta-long-dress-cut.jpg";
+
+function Header() {
+  return (
+    <header>
+      <h1>Peta Jeannie</h1>
+      <img src={mePng} />
+    </header>
+  );
+}
+
+export default Header;
---
+++ Components/Main.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <article>
+        <h2>About me</h2>
+        <p>
+          My name is Peta Jeannie. I am 24 years old. My origine is in
+          Vavatenina Which is in Analanjirofo region. I have eight siblings,
+          three brothers and five sisters.
+        </p>
+      </article>
+      <article>
+        <h2>My favorite things</h2>
+        <ol>
+          <li>
+            <b>Hobbies:</b>
+            <p>- Watching romantic movie</p>
+            <p>- Playing karaoke</p>
+            <p>- Playing music and dance</p>
+          </li>
+          <li>
+            <b>Fruits:</b> <br />I like quit most of fruits but these are my
+            favorite
+            <p>- Mangoes</p>
+            <p>- Apples</p>
+            <p>- Oranges</p>
+          </li>
+          <li>
+            <b>Food:</b>
+            <p>- Fried chicken</p>
+            <p>- Bread fruit and sweet potatoes</p>
+          </li>
+        </ol>
+        <p>
+          I am really into braiding.
+          <br />
+          In Betsimisaraka's culture, girls should know how to braid because one
+          day you will have husband then your mother in law must ask you to
+          braid,
+          <br />
+          if you don't know that, that would be embarassing.
+        </p>
+      </article>
+    </main>
+  );
+}
+
+export default Main;
